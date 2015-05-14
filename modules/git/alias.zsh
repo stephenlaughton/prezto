35b6f114c61ba45e142ff11d0a248fc5d983d63d
Stephen Laughton <stephenlaughton@mixdog.io>
2015-05-14 16:41:58 -0700

    edit gcam to add all untracked files

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -44,7 +44,7 @@ alias gbS='git show-branch -a'
 # Commit (c)
 alias gc='git commit --verbose'
 alias gca='git commit --verbose --all'
-alias gcam='git commit --verbose --all --message'
+alias gcam='git add --all && git commit --verbose --all --message'
 alias gcm='git commit --message'
 alias gco='git checkout'
 alias gcO='git checkout --patch'
