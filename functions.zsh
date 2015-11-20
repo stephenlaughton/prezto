f1888e12ff392b37d36c0684e6d47337feca4033
Stephen Laughton <stephenlaughton@gmail.com>
2015-11-20 12:42:22 -0800

    add VSCode launch function

--- functions.zsh
+++ functions.zsh
@@ -167,6 +167,11 @@ function g() {
     fi
 }
 
+# Open VSCode
+function code() {
+    VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;
+}
+
 function reload!() {
     source ~/.zshrc
 }
